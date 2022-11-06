703b2de442726a0fbe58ef8adbe16a17d961b89a
gani0325 <76983526+gani0325@users.noreply.github.com>
2022-11-06 13:55:18 +0000

    [Feat] VIdeo streaming

---
+++ IoT/21_cv2_video.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(192.168.35.26)
+if cap.isOpened() :
+    print("width : ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print("height : ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("fps : ", cap.get(cv2.CAP_PROP_FPS))
+
+while cap.isOpened() :
+    ret, img = cap.read()
+    if ret :
+        cv2.imshow("VIdeo Capture", img)
+        key = cv2.waitKey(1) &0xFF
+        if key == 27 :
+            break
+
+cap.release()
+cv2.destroyALlwindows()
